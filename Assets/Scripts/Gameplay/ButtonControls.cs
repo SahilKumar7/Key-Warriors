6a69b5033c1f2d94e5ea771abb0a9c9a329634ef
Sahil Kumar <sahil.kumar5729@gmail.com>
2021-09-18 12:14:54 +0000

    Tile button controls

---
+++ Assets/Scripts/Gameplay/ButtonControls.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonConstrols : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    
+    public Sprite inactiveSpriteImage;
+    public Sprite activeSpriteImage;
+
+    public KeyCode key;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // switch between active and inactive states on key press
+        if (Input.GetKeyDown(key))
+        {
+            spriteRenderer.sprite = activeSpriteImage;
+        }
+
+        if (Input.GetKeyUp(key))
+        {
+            spriteRenderer.sprite = inactiveSpriteImage;
+        }
+    }
+}
